8bd243c3cb452a6861c855a0816c2859ad2ec132
bluefrog1413 <162390173+bluefrog1413@users.noreply.github.com>
2024-03-27 01:00:54 +0000

    수정본

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
-x= st.slider("select a value")
-st.write(x, 'is a square', x * x)
+rand=np.random.normal(1,2, size=20)
+fig,ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
